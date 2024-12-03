494430cd411b0cafd824e78ce2b1c90932c8a728
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-12-03 21:06:14 +0530

    move to oop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/*
+* APP: 1.0.0
+*//*
 const btn = document.getElementById("submit");
 btn.addEventListener("click", e => {
     e.preventDefault();
@@ -29,4 +32,5 @@ btn.addEventListener("click", e => {
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     // xhr.send("name="+name+"&email="+email+"&password="+password+"&submit=submit");
     xhr.send(postData);
-});
+}); */
+alert('i');
